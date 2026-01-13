89773c0bd78263a70b6911a632c59cce086c52a8
Rajesh3985 <bzaitcell@gmail.com>
2026-01-13 15:58:54 +0530

    routes
    home component created
    routes created

--- frontendDevelopment/src/app/app.routes.ts
+++ frontendDevelopment/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'Home', component: Home }
+];
+
