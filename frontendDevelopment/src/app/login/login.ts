75b2e3c0a6932756d6e68c1fd6e86e36d0ec83d1
Rajesh3985 <bzaitcell@gmail.com>
2026-01-13 14:38:31 +0530

    created service
    service file created

--- frontendDevelopment/src/app/login/login.ts
+++ frontendDevelopment/src/app/login/login.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css'],
+  imports: [CommonModule, FormsModule]
 })
-export class Login {
+export class LoginComponent {
 
+  username = '';
+  password = '';
+  errorMsg = '';
+  showPassword = false;
+
+  constructor(private api: ApiService, private router: Router) {}
+  login(): void {
+    this.api.login({
+      username: this.username,
+      password: this.password
+    }).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: () => this.errorMsg = 'Invalid Username or Password'
+    });
+  }
 }
