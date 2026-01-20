fe092e26a944d265558471529dbd901f321d3ff9
Rajesh3985 <bzaitcell@gmail.com>
2026-01-20 11:00:44 +0530

    created all pages
    created uts,prs,nfr

--- frontendDevelopment/src/app/app.routes.ts
+++ frontendDevelopment/src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { Goods } from './goods/goods';
 import { Parcels } from './parcels/parcels';
 import { Earnings } from './earnings/earnings';
 import { Contracts1 } from './contracts1/contracts1';
+import { Contracts2 } from './contracts2/contracts2';
+import { NFR } from './nfr/nfr';
+import { Uts } from './uts/uts';
+import { Prs } from './prs/prs';
 
 export const routes: Routes = [
 
@@ -18,8 +22,11 @@ export const routes: Routes = [
   { path: 'goods', component: Goods },
   { path: 'parcels', component: Parcels },
   { path: 'earnings', component: Earnings },
-   { path: 'contracts1', component: Contracts1 },
-  
+  { path: 'contracts1', component: Contracts1 },
+  { path: 'contracts2', component: Contracts2 },
+  { path: 'nfr', component: NFR },
+  { path: 'uts', component: Uts },
+   { path: 'prs', component: Prs },
   // Wildcard fallback
   { path: '**', redirectTo: '' }
 ];
--- frontendDevelopment/src/app/uts/uts.ts
+++ frontendDevelopment/src/app/uts/uts.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-uts',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './uts.html',
   styleUrl: './uts.css',
 })
 export class Uts {
+ currentPage: string = 'home';
 
+  navigate(page: string) {
+    this.currentPage = page;
+  }
 }
